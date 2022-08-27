ffa48d20a298e0518c539376f057567e4b2fd00f
sadia136310 <sadia1363@gmail.com>
2022-08-27 09:37:54 +0600

    first commit

---
+++ problem1.js
@@ -0,0 +1,86 @@
+// ১. কখন const আর কখন let দিয়ে ভেরিয়েবল ডিক্লেয়ার করতে হয় সেটা তোমাকে জানতেই হবে। তুমি নিজে নিজে একটা const দিয়ে আরেকটা let দিয়ে ভেরিয়েবল ডিক্লেয়ার করে ফেলো।
+const name = 'sadia';
+let age = 23;
+age = 24;
+
+
+// ২. টেম্পলেট স্ট্রিং দিয়ে একটা স্ট্রিং তৈরি করো। সেটার মধ্যে উপরে ডিক্লেয়ার করা ভেরিয়েবল এর মান ডাইনামিক ভাবে বসাও। একইভাবে উপরে একটা অবজেক্ট ডিক্লেয়ার করো। এবং ডাইনামিকভাবে উপরের অবজেক্ট এর দুইটা প্রপার্টি এর মান তোমার টেমপ্লেট স্ট্রিং এর মধ্যে বসাও। 
+const myDetails = `
+ My name is ${name}. 
+ And my current age is ${age}
+ `;
+console.log(myDetails);
+
+
+// ৩.১ একটা প্যারামিটার ওয়ালা arrow ফাংশন ডিক্লেয়ার করো। এবং সেই ফাংশনের কাজ হবে তুমি কোন ইনপুট দিলে সেই ইনপুট সংখ্যাকে ৫ দিয়ে ভাগ করে আউটপুট দিবে। 
+const divisibleByFive = (n) => (n / 5);
+console.log(divisibleByFive(15));
+
+// ৩.২ তুমি দুইটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন লিখবে। সেই ফাংশনের ভিতরে কাজ হবে। প্রত্যেকটা ইনপুট প্যারামিটার এর সাথে ২ যোগ করবে তারপর যোগফল দুইটা গুণ করবে। ক্যামনে করবে সেটা চিন্তা করে বের করার চেষ্টা করো 
+
+const doMath = (first, second) => (first + 2) * (second + 2);
+
+const resultIs = doMath(3, 4);
+console.log(resultIs);
+
+// ৩.৩ এইবার তিনটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন ডিক্লেয়ার করো। যেই ফাংশনের কাজ হবে তিনটা প্যারামিটার নিয়ে সেই তিনটা প্যারামিটারকে গুণ করে সেই রেজাল্ট রিটার্ন করবে। 
+
+const threePara = (first, second, third) => {
+    threeParaResult = first * second * third;
+    return threeParaResult;
+}
+const myResult = threePara(2, 3, 4)
+console.log(myResult);
+
+// ৩.৪ এইবার দুইটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন নিবে। ওই arrow ফাংশনটা হবে অনেকগুলা লাইনের। সেখানে প্রত্যেকটা ইনপুট প্যারামিটার এর সাথে ২ যোগ করবে তারপর যোগফল দুইটা গুণ করবে। ক্যামনে করবে সেটা চিন্তা করে বের করার চেষ্টা করো। 
+const twoPara = (num1, num2) => {
+    firstSum = num1 + 2;
+    secondSum = num2 + 2;
+    totalSum = firstSum + secondSum;
+    mult = firstSum * secondSum;
+    return mult;
+}
+const multiplicationIs = twoPara(7, 4);
+console.log(multiplicationIs);
+
+
+// ৫. অনেকগুলা সংখ্যার একটা array হবে। তারপর তোমার কাজ হবে array এর উপরে map ইউজ করে। প্রত্যেকটা উপাদানকে ৫ দিয়ে গুন্ করে গুনফল আরেকটা array হিসেবে রাখবে। পুরা কাজটা এক লাইনে হবে। 
+
+const numbers = [2, 4, 3, 6, 1, 7, 10];
+const AfterUsingMap = numbers.map(number => number * 5);
+console.log(AfterUsingMap);
+
+
+// 6. অনেকগুলা সংখ্যার একটা array থেকে শুধু বিজোড় সংখ্যা বের করে নিয়ে আসার জন্য filter ইউজ করো 
+const numbers2 = [2, 4, 3, 6, 1, 7, 10];
+const odd = numbers2.filter(n => n % 2 == 1);
+console.log(odd);
+
+// ৭. একটা array এর মধ্যে অনেকগুলা অবজেক্ট আছে। সেখানে যেই অবজেক্ট এর price আছে ৫০০০ টেক্কা সেই অবজেক্টকে find দিয়ে বের করো। 
+
+const phone = [
+    { name: 'Nokia', price1: 50000 },
+    { name: 'Nok', price1: 50120 },
+    { name: 'Noki', price1: 76500 },
+    { name: 'Nokisa', price1: 2500 },
+    { name: 'Nokiaiu', price1: 1232500 },
+    { name: 'Nokiadde', price1: 3500 }
+
+];
+const price = phone.find(p => p.price1 < 5000);
+console.log(price);
+
+// ৯.[চ্যালেঞ্জিং] array এর destructuring করবে আর সেটা করার জন্য তুমি এরে এর থার্ড পজিশন এর উপাদান কে destructuring করে 'three' নামক একটা ভেরিয়েবল এ রাখবে। 
+const [a, b, three] = [20, 40, 50];
+console.log(three);
+
+// ১০. তিনটা প্যারামিটার ওয়ালা একটা ফাংশন লিখবে। যেই ফাংশনের কাজ হবে তিনটা প্যারামিটার নিয়ে সেই তিনটা প্যারামিটার এর যোগ করে যোগফল রিটার্ন করবে। আর থার্ড প্যারামিটার এর একটা ডিফল্ট ভ্যালু থাকবে। সেটা হবে ৭। 
+const threeParaFunction = (first, second, third = 7) => {
+    sum = first + second + third;
+    return sum;
+}
+const sumResult = threeParaFunction(2, 4, 6);
+console.log(sumResult);
+
+
+// ১১. একটা nested অবজেক্ট ডিক্লেয়ার করো(অর্থাৎ একটা অবজেক্ট এর প্রপার্টি এর মধ্যেও যে অবজেক্ট থাকতে পারে। আবার সেই অবজেক্ট এর প্রপার্টি এর মধ্যেও সে অবজেক্ট থাকতে পারে। সেই রকম একটা অবজেক্ট ডিক্লেয়ার করো। এবং যেকোন একটা প্রপার্টি এর মান একটা array হবে। জাস্ট এমন একটা অবজেক্ট)
